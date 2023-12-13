af92e9760f84064edab3f25efe22c50888921626
OborinaLyubov <lvoborina@yandex.ru>
2023-12-13 19:46:24 +0400

    Базовая реализация отправки email-сообщений для Yandex

---
+++ EmailSenderLib/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailSenderLib
+{
+    public class Constants
+    {
+        public const string RecipientNotFound = "Не указан почтовый ящик получателя";
+
+        public const string ClientNotFound = "Не указан почтовый клиент";
+
+        public const string SubjectNotFound = "Не указана тема сообщения";
+
+        public const string BodyNotFound = "Не указано тело сообщения";
+    }
+}
---
+++ EmailSenderLib/Exceptions.cs
@@ -0,0 +1,25 @@
+﻿namespace EmailSenderLib
+{
+    public class Exceptions
+    {
+        public static ArgumentNullException RecipientNotFound()
+        {
+            return new ArgumentNullException(Constants.RecipientNotFound);
+        }
+
+        public static ArgumentNullException ClientNotFound() 
+        {
+            return new ArgumentNullException(Constants.ClientNotFound);
+        }
+
+        public static ArgumentNullException SubjectNotFound() 
+        {
+            return new ArgumentNullException(Constants.SubjectNotFound);
+        }
+
+        public static ArgumentNullException BodyNotFound() 
+        {
+            return new ArgumentNullException(Constants.BodyNotFound);
+        }
+    }
+}
