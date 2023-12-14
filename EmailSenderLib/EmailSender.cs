384c25ab34307945486d5c23b11619671c6b9dea
OborinaLyubov <lvoborina@yandex.ru>
2023-12-14 11:20:53 +0400

    Реализация отправки сообщений для Mail.ru

--- EmailSenderLib/EmailSender.cs
+++ EmailSenderLib/EmailSender.cs
@@ -94,5 +94,50 @@ namespace EmailSenderLib
                 throw new Exception(ex.ToString());
             }
         }
+
+        public static void SendMail(string recipient, string client, string subject, string body)
+        {
+            if (recipient == null)
+            {
+                throw Exceptions.RecipientNotFound();
+            }
+
+            if (client == null)
+            {
+                throw Exceptions.ClientNotFound();
+            }
+
+            if (subject == null)
+            {
+                throw Exceptions.SubjectNotFound();
+            }
+
+            if (body == null)
+            {
+                throw Exceptions.BodyNotFound();
+            }
+
+            try
+            {
+                var emailSender = new MailAddress(HideData.AdminMailEmail, Constants.SenderName);
+                var emailRecipient = new MailAddress(recipient);
+                var emailMessage = new MailMessage(emailSender, emailRecipient)
+                {
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                };
+                var smtp = new SmtpClient(client, 587)
+                {
+                    Credentials = new NetworkCredential(HideData.AdminMailEmail, HideData.AdminMailPassword),
+                    EnableSsl = true
+                };
+                smtp.Send(emailMessage);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
     }
 }
