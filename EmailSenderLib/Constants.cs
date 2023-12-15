a749cb3d9a03e786fc2eda389e43be31b73436bd
OborinaLyubov <lvoborina@yandex.ru>
2023-12-15 12:02:09 +0400

    Реализация универсального подхода по выбору отправки с сервера email-сообщений

--- EmailSenderLib/Constants.cs
+++ EmailSenderLib/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string RecipientNotFound = "Не указан почтовый ящик получателя";
+        public const string RecipientEmailNotFound = "Не указан почтовый ящик получателя";
 
         public const string ClientNotFound = "Не указан почтовый клиент";
 
@@ -11,6 +11,28 @@
         public const string BodyNotFound = "Не указано тело сообщения";
 
         public const string SenderName = "Administrator";
+
+        public const string EmailNotFound = "Не указан почтовый ящик";
+
+        public const string UserNameNotFound = "Не указано имя пользователя";
+
+        public const string HostNotFound = "Не указан хост почтового сервера";
+
+        public const string PortNotFound = "Не указан порт почтового сервера";
+
+        public const string SenderEmailNotFound = "Не указан почтовый ящик отправителя";
+
+        public const string MailMessageNotFound = "Сообщение пустое";
+
+        public const string MailProviderNotFound = "Почтовый провайдер не найден";
+
+        public const string NetworkCredentialsNotFound = "Учетные данные не найдены";
+
+        public const string EmailAddressValidateError = "Указан некорректный почтовый ящик";
+
+        public const string SmtpProtocolName = "smtp";
+
+        public const string HostValidateError = "Указан некорректный хост почтового сервера";
     }
 
     public class MailPort
@@ -28,6 +50,6 @@
 
         public const string Mail = "smtp.mail.ru";
 
-        public const string Outlook = "smtp-mail.outlook.com";
+        public const string Outlook = "smtp.office365.com";
     }
 }
--- EmailSenderLib/Exceptions.cs
+++ EmailSenderLib/Exceptions.cs
@@ -1,10 +1,12 @@
-﻿namespace EmailSenderLib
+﻿using System.Diagnostics;
+
+namespace EmailSenderLib
 {
     public class Exceptions
     {
-        public static ArgumentNullException RecipientNotFound()
+        public static ArgumentNullException RecipientEmailNotFound()
         {
-            return new ArgumentNullException(Constants.RecipientNotFound);
+            return new ArgumentNullException(Constants.RecipientEmailNotFound);
         }
 
         public static ArgumentNullException ClientNotFound() 
@@ -20,6 +22,56 @@
         public static ArgumentNullException BodyNotFound() 
         {
             return new ArgumentNullException(Constants.BodyNotFound);
+        }
+
+        public static ArgumentNullException UserNameNotFound()
+        {
+            return new ArgumentNullException(Constants.UserNameNotFound);
+        }
+
+        public static ArgumentNullException EmailNotFound()
+        {
+            return new ArgumentNullException(Constants.EmailNotFound);
+        }
+
+        public static ArgumentNullException HostNotFound()
+        {
+            return new ArgumentNullException(Constants.HostNotFound);
+        }
+
+        public static ArgumentNullException PortNotFound()
+        {
+            return new ArgumentNullException(Constants.PortNotFound);
+        }
+
+        public static ArgumentException SenderAddressNotFound()
+        {
+            return new ArgumentException(Constants.SenderEmailNotFound);
+        }
+
+        public static ArgumentException MailMessageNotFound()
+        {
+            return new ArgumentException(Constants.MailMessageNotFound);
+        }
+
+        public static ArgumentException MailProviderNotFound()
+        {
+            return new ArgumentException(Constants.MailProviderNotFound);
+        }
+
+        public static ArgumentException NetworkCredentialsNotFound()
+        {
+            return new ArgumentException(Constants.NetworkCredentialsNotFound);
+        }
+
+        public static ArgumentException EmailValueValidateError()
+        {
+            return new ArgumentException(Constants.EmailAddressValidateError);
+        }
+
+        public static ArgumentException HostValidateError()
+        {
+            return new ArgumentException(Constants.HostValidateError);
         }
     }
 }
